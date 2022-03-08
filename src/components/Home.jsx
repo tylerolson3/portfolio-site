6f5b2de1dab37b81fc250d7ea337db0b3cc67c03
tylerolson3 <67395239+tylerolson3@users.noreply.github.com>
2022-03-08 10:42:45 -0600

    edit Home details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,10 +20,10 @@ const Home = () => {
             <h3 className="name" style={{ color: "#0967d2" }}>
               Tyler Olson{" "}
             </h3>
-            <h3 className="name">FrontEnd Dev</h3>
+            <h3 className="name">Web Dev</h3>
             <p className="job" style={{ fontSize: "1.3rem" }}>
-              I love exploring and creating, I'm a lifelong learner with a
-              passion for code and web development.
+              I'm a software engineer who designs web sites and web applications
+              for clients in the Kansas City area.
             </p>
             {/* END JOB */}
             <Social />
